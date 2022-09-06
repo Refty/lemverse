cda24b3b0160b8315806f31dc02534622bc6ed21
ykoeth <yann.koeth@gmail.com>
2022-09-06 16:14:50 +0200

    Add a name baseline
    Add a new 'Baseline' setting which allows to set a baseline with a
    smaller font size under the user name.

--- core/client/components/character-name-text.js
+++ core/client/components/character-name-text.js
@@ -1,26 +1,50 @@
 import Phaser from 'phaser';
 
-const style = {
+const nameStyle = {
   font: 'Verdana, "Times New Roman", Tahoma, serif',
   fontSize: 18,
   strokeColor: '#000',
   strokeSize: 3,
 };
 
+const baselineStyle = {
+  ...nameStyle,
+  fontSize: 12,
+};
+
 const defaultTint = 0xFFFFFF;
+const baselineOffset = 20;
 
-class CharacterNameText extends Phaser.GameObjects.Text {
-  constructor(scene, text, tintName) {
-    super(scene, 0, 0, text);
+class CharacterNameText {
+  constructor(scene, name, baseline, tintName) {
+    this.scene = scene;
+    this.name = this.createText(name, nameStyle);
+    this.setBaseline(baseline);
+    this.setTintFromName(tintName);
+  }
 
-    this.setFontFamily(style.font)
+  createText(message, style) {
+    const text = new Phaser.GameObjects.Text(this.scene, 0, 0, message);
+    text.setFontFamily(style.font)
       .setFontSize(style.fontSize)
       .setStroke(style.strokeColor, style.strokeSize)
       .setDepth(99999)
-      .setOrigin(0.5)
-      .setTintFromName(tintName);
+      .setOrigin(0.5);
 
-    this.scene.add.existing(this);
+    this.scene.add.existing(text);
+    return text;
+  }
+
+  setBaseline(text) {
+    if (!text) {
+      this.destroyBaseline();
+      return;
+    }
+    if (this.baseline) {
+      this.baseline.setText(text, baselineStyle);
+      return;
+    }
+    this.baseline = this.createText(text, baselineStyle);
   }
 
   setTintFromName(tintName) {
@@ -28,7 +52,34 @@ class CharacterNameText extends Phaser.GameObjects.Text {
     if (!colors) return this;
 
     const color = colors[tintName] || [defaultTint];
-    return this.setTint(...color);
+    this.name.setTint(...color);
+    if (this.baseline) {
+      this.baseline.setTint(...color);
+    }
+    return this;
+  }
+
+  setText(name, baseline) {
+    this.name.setText(name);
+    this.setBaseline(baseline);
+    return this;
+  }
+
+  setPosition(x, y) {
+    this.name.setPosition(x, this.baseline ? y - baselineOffset : y);
+    if (this.baseline) this.baseline.setPosition(x, y);
+  }
+
+  destroyBaseline() {
+    if (this.baseline) {
+      this.baseline.destroy();
+      this.baseline = undefined;
+    }
+  }
+
+  destroy() {
+    this.name.destroy();
+    this.destroyBaseline();
   }
 }
 
