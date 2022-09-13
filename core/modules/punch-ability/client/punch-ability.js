719b5e11d0af095cdfa7bb513e1ef1ddcaa8f1b3
Alim TUNC <alim@refty.co>
2022-09-13 18:16:33 +0200

    Add settings to configure features on level editor
    There are features that level admins may want to enable or disable on their
    level, so here we're giving them the chance to do just that with:
    
    * the voice amplifier
    * the global chat
    * punches

--- core/modules/punch-ability/client/punch-ability.js
+++ core/modules/punch-ability/client/punch-ability.js
@@ -1,6 +1,5 @@
 import audioManager from '../../../client/audio-manager';
-import networkManager from '../../../client/network-manager';
-import { randomFloatInRange } from '../../../lib/misc';
+import { randomFloatInRange, canUseLevelFeature } from '../../../lib/misc';
 
 const playPunchAnimation = () => {
   userManager.scene.cameras.main.shake(250, 0.015, 0.02);
@@ -29,6 +28,11 @@ window.addEventListener('load', () => {
   });
 
   hotkeys('x', { scope: scopes.player }, e => {
+    const user = Meteor.user({ fields: { _id: 1, 'profile.levelId': 1, roles: 1 } });
+    if (!user || !canUseLevelFeature(user, 'punch')) return;
+
+    e.preventDefault();
+    e.stopPropagation();
     if (e.repeat) return;
 
     punch(Object.values(userProximitySensor.nearUsers));
